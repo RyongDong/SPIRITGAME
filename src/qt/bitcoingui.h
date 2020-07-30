b12fc5fe220358fdf1d2f3bd0880070cb7b3bcf6
RyongDong <114422619+RyongDong@users.noreply.github.com>
2020-07-30 12:31:41 +0000

    Updated intro

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -29,4 +29,81 @@ QT_END_NAMESPACE
 
 /**
   Bitcoin GUI main class. This class represents the main window of the Bitcoin UI. It communicates with both the client and
-  wallet models to give the user an up-to-date view of the current c
+  wallet models to give the user an up-to-date view of the current core state.
+*/
+class BitcoinGUI : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit BitcoinGUI(QWidget *parent = 0);
+    ~BitcoinGUI();
+
+    /** Set the client model.
+        The client model represents the part of the core that communicates with the P2P network, and is wallet-agnostic.
+    */
+    void setClientModel(ClientModel *clientModel);
+    /** Set the wallet model.
+        The wallet model represents a bitcoin wallet, and offers access to the list of transactions, address book and sending
+        functionality.
+    */
+    void setWalletModel(WalletModel *walletModel);
+
+protected:
+    void changeEvent(QEvent *e);
+    void closeEvent(QCloseEvent *event);
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+
+private:
+    ClientModel *clientModel;
+    WalletModel *walletModel;
+
+    QStackedWidget *centralWidget;
+
+    OverviewPage *overviewPage;
+    MiningPage *miningPage;
+    QWidget *transactionsPage;
+    AddressBookPage *addressBookPage;
+    AddressBookPage *receiveCoinsPage;
+    SendCoinsDialog *sendCoinsPage;
+    SignVerifyMessageDialog *signVerifyMessageDialog;
+
+    QLabel *labelEncryptionIcon;
+    QLabel *labelMiningIcon;
+    QLabel *labelConnectionsIcon;
+    QLabel *labelBlocksIcon;
+    QLabel *progressBarLabel;
+    QProgressBar *progressBar;
+
+    QMenuBar *appMenuBar;
+    QAction *overviewAction;
+    QAction *miningAction;
+    QAction *historyAction;
+    QAction *quitAction;
+    QAction *sendCoinsAction;
+    QAction *addressBookAction;
+    QAction *signMessageAction;
+    QAction *verifyMessageAction;
+    QAction *firstClassMessagingAction;
+    QAction *aboutAction;
+    QAction *receiveCoinsAction;
+    QAction *optionsAction;
+    QAction *toggleHideAction;
+    QAction *exportAction;
+    QAction *encryptWalletAction;
+    QAction *backupWalletAction;
+    QAction *changePassphraseAction;
+    QAction *aboutQtAction;
+    QAction *openRPCConsoleAction;
+
+    QSystemTrayIcon *trayIcon;
+    Notificator *notificator;
+    TransactionView *transactionView;
+    RPCConsole *rpcConsole;
+
+    QMovie *syncIconMovie;
+
+    /** Create the main UI actions. */
+    void createActions();
+    /** Create the menu bar and submenus. */
+    void crea
