e8f74f0dc4d14c51659507041c3b803f7fcddf8a
RyongDong <114422619+RyongDong@users.noreply.github.com>
2020-09-09 07:10:36 +0000

    Edited via journster.com

--- src/qt/locale/bitcoin_ca_ES.ts
+++ src/qt/locale/bitcoin_ca_ES.ts
@@ -1,1 +1,51 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS languag
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ca_ES" version="2.0">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="14"/>
+        <source>About Bitcoin</source>
+        <translation>Sobre Bitcoin</translation>
+    </message>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="53"/>
+        <source>&lt;b&gt;Bitcoin&lt;/b&gt; version</source>
+        <translation>&lt;b&gt;Bitcoin&lt;/b&gt; versió</translation>
+    </message>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="97"/>
+        <source>Copyright © 2009-2012 Bitcoin Developers
+
+This is experimental software.
+
+Distributed under the MIT/X11 software license, see the accompanying file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (http://www.openssl.org/) and cryptographic software written by Eric Young (eay@cryptsoft.com) and UPnP software written by Thomas Bernard.</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="14"/>
+        <source>Address Book</source>
+        <translation>llibreta d&apos;adreces</translation>
+    </message>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="20"/>
+        <source>These are your Bitcoin addresses for receiving payments.  You may want to give a different one to each sender so you can keep track of who is paying you.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="36"/>
+        <source>Double-click to edit address or label</source>
+        <translation>Feu doble clic per editar la direcció o l&apos;etiqueta</translation>
+    </message>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="63"/>
+        <source>Create a new address</source>
+        <translation>Crear una nova adreça</translation>
+    </message>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="77"/>
+        <source>Copy the currently selected address to the
