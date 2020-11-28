4930fb8212215691e44078ba1c8f8938329278cc
RyongDong <114422619+RyongDong@users.noreply.github.com>
2020-11-28 06:51:34 +0000

    deleted unneeded files

---
+++ src/qt/locale/bitcoin_fi.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.0">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="14"/>
+        <source>About Bitcoin</source>
+        <translation>Tietoa Bitcoinista</translation>
+    </message>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="53"/>
+        <source>&lt;b&gt;Bitcoin&lt;/b&gt; version</source>
+        <translation>&lt;b&gt;Bitcoin&lt;/b&gt; versio</translation>
+    </message>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="97"/>
+        <source>Copyright © 2009-2012 Bitcoin Developers
+
+This is experimental software.
+
+Distributed under the MIT/X11 software license, see the accompanying file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
+This product includes 
