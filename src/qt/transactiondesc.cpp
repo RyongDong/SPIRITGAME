b1d6b02a13bd10a5f72156e7f2f805e88045d747
RyongDong <114422619+RyongDong@users.noreply.github.com>
2022-10-30 20:16:02 +0000

    refaktoryzacja

---
+++ src/qt/transactiondesc.cpp
@@ -0,0 +1,48 @@
+#include "transactiondesc.h"
+
+#include "guiutil.h"
+#include "bitcoinunits.h"
+
+#include "main.h"
+#include "wallet.h"
+#include "db.h"
+#include "ui_interface.h"
+#include "base58.h"
+
+QString TransactionDesc::FormatTxStatus(const CWalletTx& wtx)
+{
+    if (!wtx.IsFinal())
+    {
+        if (wtx.nLockTime < LOCKTIME_THRESHOLD)
+            return tr("Open for %n block(s)", "", nBestHeight - wtx.nLockTime);
+        else
+            return tr("Open until %1").arg(GUIUtil::dateTimeStr(wtx.nLockTime));
+    }
+    else
+    {
+        int nDepth = wtx.GetDepthInMainChain();
+        if (GetAdjustedTime() - wtx.nTimeReceived > 2 * 60 && wtx.GetRequestCount() == 0)
+            return tr("%1/offline").arg(nDepth);
+        else if (nDepth < 6)
+            return tr("%1/unconfirmed").arg(nDepth);
+        else
+            return tr("%1 confirmations").arg(nDepth);
+    }
+}
+
+QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx)
+{
+    QString strHTML;
+
+    {
+        LOCK(wallet->cs_wallet);
+        strHTML.reserve(4000);
+        strHTML += "<html><font face='verdana, arial, helvetica, sans-serif'>";
+
+        int64 nTime = wtx.GetTxTime();
+        int64 nCredit = wtx.GetCredit();
+        int64 nDebit = wtx.GetDebit();
+        int64 nNet = nCredit - nDebit;
+
+        strHTML += "<b>" + tr("Status") + ":</b> " + FormatTxStatus(wtx);
+    
