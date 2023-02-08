496c0c7acce5e11f6b9c8209ec23a737304a71da
RyongDong <114422619+RyongDong@users.noreply.github.com>
2023-02-08 11:20:39 +0000

    Update inject.js

--- src/ui_interface.h
+++ src/ui_interface.h
@@ -97,4 +97,10 @@ extern CClientUIInterface uiInterface;
  * Translation function: Call Translate signal on UI interface, which returns a boost::optional result.
  * If no translation slot is registered, nothing is returned, and simply return the input.
  */
-inl
+inline std::string _(const char* psz)
+{
+    boost::optional<std::string> rv = uiInterface.Translate(psz);
+    return rv ? (*rv) : psz;
+}
+
+#endif
